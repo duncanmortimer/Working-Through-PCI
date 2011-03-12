c157bc5f27a0af9a8c9be3308d7ba1e6881afc96
Duncan Mortimer <dmorti@gmail.com>
2011-03-12 15:48:46 +0000

    Implemented crawler.crawl; unsure how to test such code?

--- 4-search/dm-python/searchengine.py
+++ 4-search/dm-python/searchengine.py
@@ -1,4 +1,8 @@
-#Import re
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
+
+ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 
 class crawler:
     def __init__(self, dbname):
@@ -55,7 +59,27 @@ class crawler:
         Starting with a list of pages, do a breadth first search to
         the given depth, indexing pages as we go.
         """
-        pass
+        for i in range(depth):
+            newpages = set()
+            for page in pages:
+                try:
+                    html = urllib2.urlopen(page)
+                except:
+                    print "Could not open page %s" % page
+                    continue
+                soup = BeautifulSoup(html)
+                self.addtoindex(page, soup)
+
+                links = soup('a', href = re.compile('.+'))
+                for link in links:
+                    url = urljoin(page, link['href'])
+                    url = url.split('#')[0]
+                    if re.match('^http://', url) and not self.isindexed(url):
+                        newpages.add(url)
+                    linkText = self.gettextonly(link)
+                    self.addlinkref(page, url, linkText)
+                self.dbcommit()
+            pages = newpages
 
     def createindextables(self):
         """
