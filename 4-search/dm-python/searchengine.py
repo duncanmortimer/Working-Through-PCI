f9deaef04d818f4bed89bba7681b2232690fb5fd
Duncan Mortimer <dmorti@gmail.com>
2011-03-13 09:33:19 +0000

    Added searcher class to searchengine.py; notes on refactoring ideas.

--- 4-search/dm-python/searchengine.py
+++ 4-search/dm-python/searchengine.py
@@ -151,3 +151,41 @@ class crawler:
         self.db.execute("CREATE INDEX urlfromidx ON link(fromid)")
 
         self.dbcommit()
+
+class searcher:
+    def __init__(self,dbname):
+        self.db=sqlite.connect(dbname)
+
+    def __del__(self):
+        self.db.close()
+
+    def getmatchrows(self,query):
+        """Pull out all the urls containing every word in query."""
+        # Strings to build the query
+        fieldlist='w0.urlid'
+        tablelist=''
+        clauselist=''
+        wordids=[]
+        # Split the words by spaces
+        words=query.split(' ')
+        tablenumber=0
+        for word in words:
+            # Get the word ID
+            wordrow=self.db.execute(
+                "select rowid from wordlist where word='%s'" % word).fetchone()
+            if wordrow!=None:
+                wordid=wordrow[0]
+                wordids.append(wordid)
+                if tablenumber>0:
+                    tablelist+=','
+                    clauselist+=' and '
+                    clauselist+='w%d.urlid=w%d.urlid and ' % (tablenumber-1,tablenumber)
+                fieldlist+=',w%d.location' % tablenumber
+                tablelist+='wordlocation w%d' % tablenumber
+                clauselist+='w%d.wordid=%d' % (tablenumber,wordid)
+                tablenumber+=1
+        # Create the query from the separate parts
+        fullquery='select %s from %s where %s' % (fieldlist,tablelist,clauselist)
+        cur=self.db.execute(fullquery)
+        rows=[row for row in cur]
+        return rows,wordids
--- 4-search/dm-python/test/test_searchengine.py
+++ 4-search/dm-python/test/test_searchengine.py
@@ -2,38 +2,40 @@ import searchengine
 from BeautifulSoup import BeautifulSoup
 import unittest
 
-class Test(unittest.TestCase):
+class TestCrawler(unittest.TestCase):
     """
-    Unit tests for searchengine.
+    Unit tests for searchengine.crawler
     """
+
+    def setUp(self):
+        self.c = searchengine.crawler("test.db")
+
+    def tearDown(self):
+        del(self.c)
 
     def test_gettextonly_notags(self):
         """Test crawler.gettextonly: a string with no tags"""
         s = "Just a string."
         soup = BeautifulSoup(s)
-        c = searchengine.crawler("")
-        self.assertEqual(c.gettextonly(soup), s)
+        self.assertEqual(self.c.gettextonly(soup), s)
 
     def test_gettextonly_singletag(self):
         """Test crawler.gettextonly: a single html tag"""
         html = "<html>some text</html>"
         soup = BeautifulSoup(html)
-        c = searchengine.crawler("")
-        self.assertEqual(c.gettextonly(soup), "some text")
+        self.assertEqual(self.c.gettextonly(soup), "some text")
 
     def test_gettextonly_nestedtag(self):
         """Test crawler.gettextonly: nested html tags"""
         html = "<ul><li>list item 1</li><li>list item 2</li></ul>"
         soup = BeautifulSoup(html)
-        c = searchengine.crawler("")
-        self.assertEqual(c.gettextonly(soup), "list item 1\nlist item 2")
+        self.assertEqual(self.c.gettextonly(soup), "list item 1\nlist item 2")
 
     def test_getentryid_createnew(self):
         """Test crawler.getentryid: default behaviour
         when requesting a nonexistent database entry is to insert."""
-        c = searchengine.crawler("test.db")
-        idx = c.getentryid("wordlist","word","test")
-        self.assertEqual(idx, c.db.execute(
+        idx = self.c.getentryid("wordlist","word","test")
+        self.assertEqual(idx, self.c.db.execute(
             "SELECT rowid FROM wordlist WHERE word='test'"
             ).fetchone()[0])
 
@@ -41,48 +43,54 @@ class Test(unittest.TestCase):
         """Test crawler.getentryid: if requested not to
         create a new entry when a matching entry does not exist, then
         idx should be None."""
-        c = searchengine.crawler("test.db")
-        idx = c.getentryid("wordlist","word","test",createNew = False)
+        idx = self.c.getentryid("wordlist","word","test",createNew = False)
         self.assertTrue(idx is None)
 
     def test_getentryid_fetch(self):
         """Test crawler.getentryid: If entry exists,
         return its rowid."""
-        c = searchengine.crawler("test.db")
-        c.db.execute("INSERT INTO wordlist(rowid, word) VALUES (10,'test')")
-        idx = c.getentryid("wordlist","word","test")
+        self.c.db.execute("INSERT INTO wordlist(rowid, word) VALUES (10,'test')")
+        idx = self.c.getentryid("wordlist","word","test")
         self.assertEqual(idx, 10)
 
     def test_isindexed_urlnotpresent(self):
         """Test crawler.isindexed: If url not present in urllist,
         return False."""
-        c = searchengine.crawler("test.db")
-        self.assertFalse(c.isindexed("http://www.someurl.com"))
+        self.assertFalse(self.c.isindexed("http://www.someurl.com"))
 
     def test_isindexed_urlpresentnowords(self):
         """Test crawler.isindexed: If url is present in urllist, but
         there are no words associated with the url, assume it has been
         indexed incorrectly and return False."""
-        c = searchengine.crawler("test.db")
-        c.db.execute("INSERT INTO urllist (url) VALUES ('http://www.someurl.com')")
-        self.assertFalse(c.isindexed("http://www.someurl.com"))
+        self.c.db.execute("INSERT INTO urllist (url) VALUES ('http://www.someurl.com')")
+        self.assertFalse(self.c.isindexed("http://www.someurl.com"))
 
     def test_isindexed_urlpresentwithwords(self):
         """Test crawler.isindexed: If url is present in urllist, and
         there are words in wordlist associated with that url, return True."""
-        c = searchengine.crawler("test.db")
-        urlid = c.db.execute(
+        urlid = self.c.db.execute(
             "INSERT INTO urllist (url) VALUES ('http://www.someurl.com')").lastrowid
-        wordid = c.db.execute("INSERT INTO wordlist (word) VALUES ('test')").lastrowid
-        c.db.execute("INSERT INTO wordlocation (urlid, wordid, location) VALUES (%d, %d, 1)" % (urlid, wordid))
-        self.assertTrue(c.isindexed("http://www.someurl.com"))
+        wordid = self.c.db.execute("INSERT INTO wordlist (word) VALUES ('test')").lastrowid
+        self.c.db.execute("INSERT INTO wordlocation (urlid, wordid, location) VALUES (%d, %d, 1)" % (urlid, wordid))
+        self.assertTrue(self.c.isindexed("http://www.someurl.com"))
 
 
     def test_separatewords(self):
         """Test crawler.separatewords(text)."""
-        c = searchengine.crawler("")
         self.assertEqual(["the","cat","in","the","hat"],\
-                         c.separatewords("The cat in the hat."))
+                         self.c.separatewords("The cat in the hat."))
+
+class TestSearcher(unittest.TestCase):
+    """
+    Unit tests for searchengine.searcher
+    """
+
+    def setUp(self):
+        self.s = searchengine.searcher("test.db")
+
+    def tearDown(self):
+        del(self.s)
+
 
 if __name__=="__main__":
     unittest.main()
